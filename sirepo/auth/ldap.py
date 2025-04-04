b38f15f447ceb07d03a6b650bcf9db0818fdd18c
Himanshu Goel <himanshu@hgoel.dev>
2025-04-04 13:28:44 -0400

    Make the ldap parameters required without defauls

--- sirepo/auth/ldap.py
+++ sirepo/auth/ldap.py
@@ -96,13 +96,13 @@ def init_apis(*args, **kwargs):
     global _cfg
 
     _cfg = pkconfig.init(
-        server=(
-            "ldap://127.0.0.1:389", str, " ldap://ip:port"
+        server=pkconfig.Required(
+            str, " ldap://ip:port"
         ),
-        dn_suffix=(
-            ",ou=users,dc=example,dc=com", _cfg_dn_suffix, "ou and dc values of dn"
+        dn_suffix=pkconfig.Required(
+            _cfg_dn_suffix, "ou and dc values of dn"
         ),
-        dn_prefix=(
-            "mail=", _cfg_dn_suffix, "prefix from username/email of dn"
+        dn_prefix=pkconfig.Required(
+            _cfg_dn_suffix, "prefix from username/email of dn"
         ),
     )
